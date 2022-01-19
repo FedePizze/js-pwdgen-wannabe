1c9b04a8367577c4f9895c32cf8f4099c9719347
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-01-19 15:43:10 +0100

    file js

---
+++ script.js
@@ -0,0 +1,5 @@
+let chiamarsi = prompt('li tuo nome');
+let cognome = prompt('il tuo cognome');
+let colore = prompt('il tuo colore preferito');
+
+document.getElementById('name').innerHTML = "ciao " + chiamarsi + " " + cognome + " amante del colore " + colore;
